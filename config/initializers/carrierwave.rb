207048d553d0e91a73e8e4051c983e682b329de2
Taiwo Ayanleye <onlyyouandty@yahoo.com>
2014-10-11 14:06:01 +0100

    Correct carrierwave.rb file for Heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV['aws_access_key_id'],                        # required
-    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],                       # required
+    :aws_secret_access_key  => ENV['aws_secret_access_key'],                       # required
   }
   config.fog_directory  = ENV['fog_directory']                          # required
 end
