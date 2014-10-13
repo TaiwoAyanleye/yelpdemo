fa141d20d23c754d4465d8c1501c5cba41744323
Taiwo Ayanleye <onlyyouandty@yahoo.com>
2014-10-13 11:56:38 +0100

    Link Reviews to Users

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_review, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -20,6 +21,7 @@ class ReviewsController < ApplicationController
 
   def create
     @review = Review.new(review_params)
+    @review.user_id = current_user.id
     @review.save
     respond_with(@review)
   end
